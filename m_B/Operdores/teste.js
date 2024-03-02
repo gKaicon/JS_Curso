af145e39cef96cc2151b85cb975cc2f2b8f35fc5
Gabriel Káicon <gkaicon2@gmail.com>
2024-03-01 22:14:23 -0300

    Att casa 01/03 da aula 0 a aula 10 do JS

---
+++ m_B/Operdores/teste.js
@@ -0,0 +1,22 @@
+console.log("Operadores Aritimeticos");
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 3);
+console.log("\nOeradores Relacionais");
+console.log(5>2);
+console.log(7<4);
+console.log(8<=8);
+console.log(9>=7);
+console.log(5==5);
+console.log(4!=4);
+console.log(6==='6');
+console.log(7==='7');//false
+console.log(7!=='7');//eles tem tipos diferentes? sim, então true
+console.log(78+2 > 90);//false
+console.log("\nOperadores lógicos");
+console.log(!true);
+console.log(true && false);
+console.log(false || true);
